801b17fce887def8781f5366e11e6c6804237b67
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-06-14 23:39:40 +1000

    Delete obj/local/armeabi-v7a directory

--- obj/local/armeabi-v7a/objs/sampvoice/vendor/RakNet/LinuxStrings.o.d
+++
@@ -1,8 +0,0 @@
-./obj/local/armeabi-v7a/objs/sampvoice/vendor/RakNet/LinuxStrings.o: \
-  jni/vendor/RakNet/LinuxStrings.cpp \
-  C:/ndk/build//../sources/cxx-stl/llvm-libc++/include\string.h \
-  C:/ndk/build//../sources/cxx-stl/llvm-libc++/include\__config
-
-C:/ndk/build//../sources/cxx-stl/llvm-libc++/include\string.h:
-
-C:/ndk/build//../sources/cxx-stl/llvm-libc++/include\__config:
