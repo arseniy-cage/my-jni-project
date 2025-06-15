0be745e718b3847acafbf9ac91f3f32441bdb50e
arseniy-cage <lllufoxnewyear2023@gmail.com>
2025-06-15 12:58:02 +1000

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,18 +4,18 @@ LOCAL_PATH := $(call my-dir)
 # Define and include each prebuilt static library separately.
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := openal # Module name without 'lib' prefix
-LOCAL_SRC_FILES := vendor/openal/libopenal.a
+LOCAL_MODULE := openal
+LOCAL_SRC_FILES := vendor/openal/libopenal.a # Относительно LOCAL_PATH (т.е. jni/vendor/openal/libopenal.a)
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := opus # Module name without 'lib' prefix
-LOCAL_SRC_FILES := vendor/opus/libopus.a
+LOCAL_MODULE := opus
+LOCAL_SRC_FILES := vendor/opus/libopus.a # Относительно LOCAL_PATH
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := enet # Module name without 'lib' prefix
-LOCAL_SRC_FILES := vendor/enet/libenet.a
+LOCAL_MODULE := enet
+LOCAL_SRC_FILES := vendor/enet/libenet.a # Относительно LOCAL_PATH
 include $(PREBUILT_STATIC_LIBRARY)
 
 
@@ -28,48 +28,48 @@ LOCAL_LDLIBS := -llog -lOpenSLES -lGLESv2 -lEGL
 
 # Include directories for headers
 # Ensure all directories containing header files (.h) are listed here.
-# *** ИСПРАВЛЕНО: Добавлен префикс 'jni/' к путям, если заголовки находятся в my-jni-project/jni/... ***
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/jni/vendor/ini \
-                    $(LOCAL_PATH)/jni/vendor/RakNet \
-                    $(LOCAL_PATH)/jni/vendor/RakNet/SAMP \
-                    $(LOCAL_PATH)/jni/vendor/imgui \
-                    $(LOCAL_PATH)/jni/vendor/hash \
-                    $(LOCAL_PATH)/jni/voice \
-                    $(LOCAL_PATH)/jni/net \
-                    $(LOCAL_PATH)/jni/util \
-                    $(LOCAL_PATH)/jni/cryptors \
-                    $(LOCAL_PATH)/jni/game \
-                    $(LOCAL_PATH)/jni/game/GTASA/Core \
-                    $(LOCAL_PATH)/jni/game/GTASA/Models \
-                    $(LOCAL_PATH)/jni/game/RW \
-                    $(LOCAL_PATH)/jni/gtare \
-                    $(LOCAL_PATH)/jni/clientlogic \
-                    $(LOCAL_PATH)/jni/gui \
-                    $(LOCAL_PATH)/jni/santrope-tea-gtasa/encryption # IMPORTANT: This path assumes 'santrope-tea-gtasa' is directly inside your JNI root folder.
+# *** ИСПРАВЛЕНО: Убран лишний 'jni/' из всех путей ***
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/vendor/ini \
+                    $(LOCAL_PATH)/vendor/RakNet \
+                    $(LOCAL_PATH)/vendor/RakNet/SAMP \
+                    $(LOCAL_PATH)/vendor/imgui \
+                    $(LOCAL_PATH)/vendor/hash \
+                    $(LOCAL_PATH)/voice \
+                    $(LOCAL_PATH)/net \
+                    $(LOCAL_PATH)/util \
+                    $(LOCAL_PATH)/cryptors \
+                    $(LOCAL_PATH)/game \
+                    $(LOCAL_PATH)/game/GTASA/Core \
+                    $(LOCAL_PATH)/game/GTASA/Models \
+                    $(LOCAL_PATH)/game/RW \
+                    $(LOCAL_PATH)/gtare \
+                    $(LOCAL_PATH)/clientlogic \
+                    $(LOCAL_PATH)/gui \
+                    $(LOCAL_PATH)/santrope-tea-gtasa/encryption # Это путь относительно jni/
 
 # Source files for the sampvoice module
 # List all .cpp and .c files here.
-# *** ИСПРАВЛЕНО: Добавлен префикс 'jni/' ко всем путям здесь ***
+# *** ИСПРАВЛЕНО: Убран лишний 'jni/' из всех путей ***
 LOCAL_SRC_FILES := \
-    $(wildcard $(LOCAL_PATH)/jni/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/game/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/gtare/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/clientlogic/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/net/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/util/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/game/RW/RenderWare.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/gui/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/voice/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/cryptors/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/game/GTASA/Models/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/game/GTASA/Core/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/santrope-tea-gtasa/encryption/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/santrope-tea-gtasa/encryption/*.c) \
-    $(wildcard $(LOCAL_PATH)/jni/vendor/ini/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/vendor/RakNet/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/vendor/RakNet/SAMP/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/vendor/imgui/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/jni/vendor/hash/md5.cpp)
+    $(wildcard $(LOCAL_PATH)/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/gtare/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/clientlogic/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/net/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/util/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/RW/RenderWare.cpp) \
+    $(wildcard $(LOCAL_PATH)/gui/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/voice/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/cryptors/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/GTASA/Models/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/game/GTASA/Core/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/santrope-tea-gtasa/encryption/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/santrope-tea-gtasa/encryption/*.c) \
+    $(wildcard $(LOCAL_PATH)/vendor/ini/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/RakNet/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/RakNet/SAMP/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/imgui/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/vendor/hash/md5.cpp)
 
 # Link against the prebuilt static libraries defined above
 LOCAL_STATIC_LIBRARIES := openal opus enet
